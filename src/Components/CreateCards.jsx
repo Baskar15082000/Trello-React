90a9080beef944727ff9eeee26de2d32526f46cc
baskar <baskar15082000@gmail.com>
2024-01-19 12:07:24 +0530

    reducer

--- src/Components/CreateCards.jsx
+++ src/Components/CreateCards.jsx
@@ -4,11 +4,11 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useState } from "react";
 import { getCheckList } from "../Api";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import LibraryAddCheckOutlinedIcon from "@mui/icons-material/LibraryAddCheckOutlined";
 import { createCheckList, deleteCheckList } from "../Api";
 import Item from "./Item";
-import DataFetch from "./DataFetch";
+import { reduce } from "./DataFetch";
 
 const style = {
   position: "absolute",
@@ -21,30 +21,33 @@ const style = {
 };
 
 export default function CreateCards({ title, id }) {
-  const [checklist, setCheckList] = useState([]);
+  const initialState = {
+    post: [],
+    error: "",
+  };
   const [checklistName, setCheckListName] = useState("");
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [displaAdd, setDisplayAdd] = useState(false);
 
-  // useEffect(() => {
-  //   getCheckList(id).then((res) => setCheckList(res));
-  // }, []);
+  const [checklist, dispatch] = useReducer(reduce, initialState);
+  function ondelete(listId) {
+    deleteCheckList(id, listId).then((res) =>
+      dispatch({ type: "Delete", payload: res })
+    );
+  }
 
-  // function onchange(e) {
-  //   setCheckListName(e.target.value);
-  //   console.log(checklistName);
-  // }
-  // function onsubmit() {
-  //   createCheckList(checklistName, id).then((res) =>
-  //     setCheckList((pre) => [...pre, res])
-  //   );
-  //   setCheckListName("");
-  // }
-  // function deleteChecklist(listId) {
-  //   deleteCheckList(id, listId).then((res) => setCheckList(res));
-  // }
+  function onchange(e) {
+    setCheckListName(e.target.value);
+    console.log(checklistName);
+  }
+  function onsubmit() {
+    createCheckList(checklistName, id).then((res) =>
+      dispatch({ type: "Add", payload: res })
+    );
+    setCheckListName("");
+  }
 
   return (
     <div>
@@ -64,74 +67,6 @@ export default function CreateCards({ title, id }) {
           <Typography id="modal-modal-title ">
             <div style={{ fontSize: "2rem" }}>{title}</div>
           </Typography>
-
-          <div className="checklist d-flex  ">
-            <DataFetch id={id} deleteChecklist={deleteChecklist} />
-            {/* <div className="cardbox mt-4 pe-5">
-              {checklist.map((e) => {
-                return (
-                  <div className="mb-3 " key={e.id}>
-                    <div className="d-flex justify-content-between">
-                      <div>
-                        <LibraryAddCheckOutlinedIcon />
-
-                        {e.name}
-                      </div>
-
-                      <button
-                        className=" border-0 ms-5"
-                        onClick={() => deleteChecklist(e.id)}
-                      >
-                        delete
-                      </button>
-                    </div>
-                    <Item id={e.id} cardid={id} />
-                  </div>
-                );
-              })}
-            </div> */}
-            <div className="ms-4  mt-3 ">
-              <div className="d-flex flex-column  ">
-                <div>Add to card</div>
-                <div className="d-flex mt-3 mb-2">
-                  <LibraryAddCheckOutlinedIcon />
-                  <div
-                    className="ms-3"
-                    style={{ cursor: "pointer" }}
-                    onClick={() => setDisplayAdd((pre) => !pre)}
-                  >
-                    Checklist
-                  </div>
-                </div>
-              </div>
-              <div>
-                {displaAdd && (
-                  <div>
-                    <div className="d-flex flex-column">
-                      <input
-                        type="text"
-                        onChange={onchange}
-                        value={checklistName}
-                        placeholder="checklist"
-                      />
-                      <div className="d-flex my-2 px-3 bg-info rounded align-items-center justify-content-between">
-                        <button
-                          className="border-0 text-danger bg-transparent  py-0"
-                          style={{ fontSize: "1.2rem" }}
-                          onClick={() => setDisplayAdd((pre) => !pre)}
-                        >
-                          x
-                        </button>
-                        <div onClick={onsubmit} style={{ cursor: "pointer" }}>
-                          Add
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
         </Box>
       </Modal>
     </div>
---
+++ src/Components/DataFetch.js
@@ -0,0 +1,20 @@
+export const reduce = (state, action) => {
+  switch (action.type) {
+    case "Fetch_Success":
+      return {
+        post: action.payload,
+        error: "",
+      };
+    case "Error":
+      return {
+        post: [],
+        error: "error",
+      };
+    case "Delete":
+      return { post: action.payload };
+    case "Add":
+      return { post: [...state.post, action.payload] };
+    default:
+      return state;
+  }
+};
